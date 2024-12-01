5fead6297527a6e7f25ab3cf29d64b271c810cdd
ozanturhan <m.ozanturhan@hotmail.com>
2024-12-01 17:05:02 +0100

    remove a commented line

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const WebViewScreen(
         cookies: {
-/*          'next-auth.session-token': 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..qZeA0iyCOFi3aarl.GCMeczwMl3B-hrHJ9-LIcjauiPrcUIRCmVN_CBb8M52DGjnfP_5BshLTCFcQNjxDcJ0_VRvHGwk-j06sS3YndY7KDPXXNEr744B8OUkFLRqa4h9lqXis3OWiij6nRz6IxYXmeo7alMZJM_csBpZpwTwBLgcS35Fbhd6-SLjYMfe7hjFU39gXHvBil3ksCGO1QT2m8eR1wnN0JzVyzhJ1L8WAUaWZ5IQR1kF0JmmqH8GRAfSn0T96Ig1umuZqMLNHZklLq7M0AQVnnouHreXSUvtpRcQkZXB8ncOuLjCOE8P6LYJASXCXx8CaRUb74O6hWoGLttL_STthGv58NKdd__VA1n_Q8k2AjpI1h1wdllr1eUI3KiCYJR_Dizq8J1yl0yPW8gZGZ5zFHKRRne97NfD-kMz5VTcNz5kzKRL3g0n6ZIkDnWZ_wIPPrzmyKpvO0uXWBhrjNpm1G0GUQqdBCFMOZaHQ-r21PO5aXwyKZQHNZNcdAALvE3t_CV_WiwVGpkeNfe5Fv659a_gzpPnsIwfjg3pH31QazKFas8OqGTQZhvPXnlK9f1gr8r9ihyBLX17I0kaVzI5LUOaORLgTPziHAMos6fHtQAetqB1IaqXvKQFTaegJgDsFYotPUblMUttSZHe1pOv-KI7eQjCh9Fs.CVN_qLcWipn_MZJh4Xvkmw',*/
           'deviceType': 'mobile',
         },
       ),
